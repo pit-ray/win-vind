ab86438bad103e7fbc7b3f0ef6857621d9b2d657
pit-ray <pit-ray@outlook.com>
2021-01-16 23:14:26 +0900

    remove all caches of libs

--- setup_libs.bat
+++ setup_libs.bat
@@ -62,6 +62,9 @@ cd wxWidgets/build/msw
     @if exist ../../lib/vc* (
         powershell rm -r "..\\..\\lib\\vc*"
     )
+    @if exist vc_mswu* (
+        powershell rm -r vc_mswu*
+    )
 
     @if defined BUILD_64 (
         call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
@@ -73,20 +76,33 @@ cd wxWidgets/build/msw
         nmake /f makefile.vc BUILD=release SHARED=0 UNICODE=1 TARGET_CPU=X86 RUNTIME_LIBS=static
     )
 
+    @if exist vc_mswu* (
+        powershell rm -r vc_mswu*
+    )
 ) else (
     @if exist ../../lib/gcc* (
        powershell rm -r "..\\..\\lib\\gcc*"
     )
+    @if exist gcc_mswu (
+        powershell rm -r gcc_mswu
+    )
 
-   @echo - Build for 64bit -------------------------------------------------------------
-   mingw32-make -j %NUMBER_OF_PROCESSORS% -f makefile.gcc setup_h BUILD=release SHARED=0 UNICODE=1 CPPFLAGS="-m64 -w"
-   mingw32-make -j %NUMBER_OF_PROCESSORS% -f makefile.gcc BUILD=release SHARED=0 UNICODE=1 CPPFLAGS="-m64 -w"
-   @rem powershell mv "..\\..\\lib\\gcc_lib" "..\\..\\lib\\gcc_x64_lib"
+    @echo - Build for 64bit -------------------------------------------------------------
+    mingw32-make -j %NUMBER_OF_PROCESSORS% -f makefile.gcc setup_h BUILD=release SHARED=0 UNICODE=1 CPPFLAGS="-m64 -w"
+    mingw32-make -j %NUMBER_OF_PROCESSORS% -f makefile.gcc BUILD=release SHARED=0 UNICODE=1 CPPFLAGS="-m64 -w"
+    @rem powershell mv "..\\..\\lib\\gcc_lib" "..\\..\\lib\\gcc_x64_lib"
+    @rem
+    @if exist gcc_mswu (
+        powershell rm -r gcc_mswu
+    )
 
-   @rem powershell rm -r gcc_mswu*
-   @rem @echo - Build for 32bit -------------------------------------------------------------
-   @rem mingw32-make -j %NUMBER_OF_PROCESSORS% -f makefile.gcc setup_h BUILD=release SHARED=0 UNICODE=1 CPPFLAGS="-m32 -w"
-   @rem mingw32-make -j %NUMBER_OF_PROCESSORS% -f makefile.gcc BUILD=release SHARED=0 UNICODE=1 CPPFLAGS="-m32 -w"
+    @rem @echo - Build for 32bit -------------------------------------------------------------
+    @rem mingw32-make -j %NUMBER_OF_PROCESSORS% -f makefile.gcc setup_h BUILD=release SHARED=0 UNICODE=1 CPPFLAGS="-m32 -w"
+    @rem mingw32-make -j %NUMBER_OF_PROCESSORS% -f makefile.gcc BUILD=release SHARED=0 UNICODE=1 CPPFLAGS="-m32 -w"
+    @rem powershell rm -r gcc_mswu
+    @if exist gcc_mswu (
+       powershell rm -r gcc_mswu
+    )
 )
 
 cd ../../../

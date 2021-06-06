9b25fdb0ead52e8df5985c31f796de4bdef18ee5
pit-ray <pit-ray@outlook.com>
2021-06-06 23:58:16 +0900

    fix license version in source

--- core/include/entry.hpp
+++ core/include/entry.hpp
@@ -12,7 +12,7 @@
 
 MIT License
 
-Copyright (c) 2020 pit-ray
+Copyright (c) 2020-2021 pit-ray
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights

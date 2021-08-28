1c20de39f2ecf78ef68e82ee964c573f723ab61a
pit-ray <pit-ray@outlook.com>
2021-08-29 04:21:53 +0900

    fix coverity tools installer

--- tools/install_coverity_tools.sh
+++ tools/install_coverity_tools.sh
@@ -4,5 +4,5 @@
 
 if [ ! -e cov_tools ] ; then
     wget https://scan.coverity.com/download/cxx/win64 --post-data "token=$1&project=pit-ray%2Fwin-vind" -O cov_tools.zip
-    unzip -jv cov_tools.zip -d cov_tools 2>/dev/null
+    unzip -j cov_tools.zip -d cov_tools
 fi

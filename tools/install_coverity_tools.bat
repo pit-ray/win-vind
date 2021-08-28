aed65ca0f266c5d17dc5ed107957a44011995e27
pit-ray <pit-ray@outlook.com>
2021-08-29 02:39:11 +0900

    fix token escape

--- tools/install_coverity_tools.bat
+++ tools/install_coverity_tools.bat
@@ -1,9 +1,14 @@
 @rem Assumed to be run on MSYS2
 
+@if "%1" == "" (
+    @echo Error: Invalid Coverity token
+    @exit
+)
+
 @if not exist bin (
     mkdir bin
     cd bin
-    wget https://scan.coverity.com/download/cxx/win64 --post-data "token=$TOKEN&project=pit-ray%2Fwin-vind" -O coverity_tool.zip
+    wget https://scan.coverity.com/download/cxx/win64 --post-data "token=%1&project=pit-ray%%2Fwin-vind" -O coverity_tool.zip
     unzip coverity_tool.zip
     cd ..
 )

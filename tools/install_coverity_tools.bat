dbf2622aef00ac934866f9664fa107d68706df28
pit-ray <pit-ray@outlook.com>
2021-08-29 02:49:14 +0900

    fix unzip command

--- tools/install_coverity_tools.bat
+++ tools/install_coverity_tools.bat
@@ -8,7 +8,7 @@
 @if not exist bin (
     mkdir bin
     cd bin
-    wget https://scan.coverity.com/download/cxx/win64 --post-data "token=%1&project=pit-ray%%2Fwin-vind" -O coverity_tool.zip
-    unzip coverity_tool.zip
+    wget https://scan.coverity.com/download/cxx/win64 --post-data "token=%1&project=pit-ray%%2Fwin-vind" -O cov_tools.zip
+    unzip -j cov_tools.zip -d cov_tools
     cd ..
 )

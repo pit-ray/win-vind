735f46ef25e05e1d7b382f5e94804cd9f8b81730
pit-ray <pit-ray@outlook.com>
2021-08-29 02:29:49 +0900

    change file format from shell to bat

--- tools/install_coverity_tools.bat
+++ tools/install_coverity_tools.bat
@@ -1,10 +1,9 @@
-#!/bin/bash
+@rem Assumed to be run on MSYS2
 
-# Assumed to be run on MSYS2
-if [! -e bin]; then
+@if not exist bin (
     mkdir bin
     cd bin
     wget https://scan.coverity.com/download/cxx/win64 --post-data "token=$TOKEN&project=pit-ray%2Fwin-vind" -O coverity_tool.zip
     unzip coverity_tool.zip
     cd ..
-fi
+)

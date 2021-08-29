798aa32ccbc405c5fa901b51cabf639214b1d51d
pit-ray <pit-ray@outlook.com>
2021-08-29 15:38:37 +0900

    resubmit coverity build

--- tools/download_coverity_tools.bat
+++
@@ -1,5 +0,0 @@
-@rem Assumed to be run on MSYS2
-
-@if not exist cov_tools.zip (
-    wget https://scan.coverity.com/download/cxx/win64 --post-data "token=%1&project=pit-ray%%2Fwin-vind" -O cov_tools.zip
-)

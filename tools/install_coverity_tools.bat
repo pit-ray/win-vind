5a1218240296d90b14edd70b33b05840c41fb3f2
pit-ray <pit-ray@outlook.com>
2021-08-29 14:51:36 +0900

    fix install step

--- tools/install_coverity_tools.bat
+++ tools/install_coverity_tools.bat
@@ -1,4 +1,5 @@
 if not exist cov_tools (
+    wget https://scan.coverity.com/download/cxx/win64 --post-data "token=%1&project=pit-ray%%2Fwin-vind" -O cov_tools.zip
     unzip cov_tools.zip
     mv cov-analysis-win64-*.* cov_tools
 )

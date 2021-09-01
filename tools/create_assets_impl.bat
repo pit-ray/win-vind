cbd63f60d2157100b74a68df032df8767d10a4e7
pit-ray <pit-ray@outlook.com>
2021-09-02 04:15:14 +0900

    fix build release script

--- tools/create_assets_impl.bat
+++ tools/create_assets_impl.bat
@@ -14,7 +14,7 @@
 )
 
 if exist release_%3 (
-    rmdir /s /q release_%3
+    @rem rmdir /s /q release_%3
 )
 
 if exist bin_%3 (
@@ -24,24 +24,22 @@ if exist bin_%3 (
 mkdir bin_%3
 
 @echo Create Installer Version -------------------------------------------------------
-echo 1 > ".\\res\\default_config\\instype"
+echo 1 > res\default_config\instype
 
 call build.bat -release %2 %3
 cd release_%3
 cpack . -C Release
 cd ..
-copy /Y ".\\release_%3\\setup*" ".\\bin_%3\\*_%1_%3bit.exe"
+copy /Y release_%3\setup* bin_%3\*_%1_%3bit.exe
 
 @echo Create Zip Version ----------------------------------------------------------
-echo 0 > ".\\res\\default_config\\instype"
+echo 0 > res\default_config\instype
 
-mkdir ".\\bin_%3\\win-vind"
-mkdir ".\\bin_%3\\win-vind\\default_config"
-mkdir ".\\bin_%3\\win-vind\\resources"
+mkdir bin_%3\win-vind
+copy /Y release_%3\win-vind.exe bin_%3\win-vind\
 
-copy /Y ".\\release_%3\\win-vind.exe" ".\\bin_%3\\win-vind\\win-vind.exe"
-xcopy /e /Y ".\\res\\default_config" ".\\bin_%3\\win-vind\\default_config"
-xcopy /e /Y ".\\res\\resources" ".\\bin_%3\\win-vind\\resources"
+xcopy /I /E /Y res\default_config bin_%3\win-vind\default_config
+xcopy /I /E /Y res\resources bin_%3\win-vind\resources
 
-powershell Compress-Archive -Path ".\\bin_%3\\win-vind" -DestinationPath ".\\bin_%3\\win-vind_%1_%3bit".zip"
-powershell Compress-Archive -Path ".\\bin_%3\\setup_win-vind_%1_%3bit.exe" -DestinationPath ".\\bin_%3\\setup_win-vind_%1_%3bit.exe.zip"
+powershell Compress-Archive -Path "bin_%3\win-vind" -DestinationPath "bin_%3\win-vind_%1_%3bit.zip"
+powershell Compress-Archive -Path "bin_%3\setup_win-vind_%1_%3bit.exe" -DestinationPath "bin_%3\setup_win-vind_%1_%3bit.exe.zip"

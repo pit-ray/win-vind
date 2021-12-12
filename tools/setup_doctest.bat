5039212f1e1a52878d7e1a747642d528b83076cb
pit-ray <pit-ray@outlook.com>
2021-12-13 04:47:55 +0900

    Fixed the version of doctest to 2.4.6.

--- tools/setup_doctest.bat
+++ tools/setup_doctest.bat
@@ -2,6 +2,6 @@
 @echo Setup doctest --------------------------------------------------------------------------
 echo.
 
-git clone https://github.com/onqtam/doctest.git -j %NUMBER_OF_PROCESSORS% --depth=1 tests/doctest
+git clone https://github.com/onqtam/doctest.git -j %NUMBER_OF_PROCESSORS% -b 2.4.6 --depth=1 tests/doctest
 
 @echo done.

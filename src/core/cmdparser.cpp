92c5fb3d6eec01fcd78d872aaadd212edec34f57
pit-ray <pit-ray@outlook.com>
2022-01-03 04:12:23 +0900

    fix MSVC build issue

--- src/core/cmdparser.cpp
+++ src/core/cmdparser.cpp
@@ -11,6 +11,7 @@
 
 #include <algorithm>
 #include <string>
+#include <unordered_map>
 
 
 namespace vind
--- src/core/version.hpp
+++ src/core/version.hpp
@@ -1,6 +1,6 @@
 #ifndef _VERSION_HPP
 #define _VERSION_HPP
 
-#define WIN_VIND_VERSION "4.3.1"
+#define WIN_VIND_VERSION "4.3.2"
 
 #endif

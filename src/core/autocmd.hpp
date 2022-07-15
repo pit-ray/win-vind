0b19c7476897bcc418f314e6660f05b202758a4b
pit-ray <pit-ray@outlook.com>
2022-07-15 22:33:46 +0900

    add event enum

--- src/core/autocmd.hpp
+++ src/core/autocmd.hpp
@@ -1,4 +1,37 @@
 #ifndef _AUTOCMD_HPP
 #define _AUTOCMD_HPP
 
+namespace vind
+{
+    namespace core
+    {
+        enum class AutocmdEvent {
+            WinEnter,
+            WinLeave,
+
+            // GUI
+            GNormalEnter,
+            GNormalLeave,
+            GVisualEnter,
+            GVisualLeave,
+
+            // Editor
+            ENormalEnter,
+            ENormalLeave,
+            EVisualEnter,
+            EVisualLeave,
+
+            InsertEnter,
+            InsertLeave,
+
+            ResidentEnter,
+            ResidentLeave,
+
+            CmdLineEnter,
+            CmdLineLeave
+        } ;
+
+    }
+}
+
 #endif

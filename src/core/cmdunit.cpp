1937685e12cdc4b3e07984f8cd35d00b3c4d0fcb
pit-ray <pit-ray@outlook.com>
2022-08-10 14:26:27 +0900

    add CmdUnit

--- src/core/cmdunit.cpp
+++ src/core/cmdunit.cpp
@@ -3,80 +3,180 @@
 #include "cmdparser.hpp"
 #include "keycode.hpp"
 
-#include "bind/binded_func.hpp"
+#include "bind/bindedfunc.hpp"
+#include "bind/bindinglist.hpp"
+#include "util/def.hpp"
 
 
 namespace vind
 {
     namespace core
     {
-        struct CmdUnit::Impl {
-            bool external_ ;
-
-            std::vector<KeyCode> keys_ ;
-
-            bind::BindedFunc::SPtr func_ ;
-
-            template <typename String>
-            Impl(bool external)
-            : external_(external),
-              keys_()
-            {}
-        } ;
-
         CmdUnit::CmdUnit()
-        : CmdUnit("")
+        : keycodes_()
         {}
 
-        CmdUnit::CmdUnit(std::string&& strkeyset, bool external)
-        : pimpl(std::make_unique<Impl>(external))
-        {
-            pimpl->key = parse_combined_command(std::move(strkeyset))
-        }
+        CmdUnit::CmdUnit(const Data& codes)
+        : keycodes_(codes)
+        {}
 
-        CmdUnit::CmdUnit(const std::string& strcmd, bool external)
-        : CmdUnit(std::make_unique<Impl>(external))
-        {
-            pimpl->key = parse_combined_command(strkeyset)
-        }
+        CmdUnit::CmdUnit(Data&& codes)
+        : keycodes_(std::move(codes))
+        {}
+
+        CmdUnit::CmdUnit(std::initializer_list<KeyCode>&& codes)
+        : keycodes_(codes)
+        {}
 
         CmdUnit::~CmdUnit() noexcept = default ;
+
+        CmdUnit::CmdUnit(const CmdUnit& rhs)
+        : keycodes_(rhs.keycodes_)
+        {}
+
         CmdUnit::CmdUnit(CmdUnit&&) = default ;
         CmdUnit& CmdUnit::operator=(CmdUnit&&) = default ;
 
+        CmdUnit& CmdUnit::operator=(const CmdUnit::Data& rhs) {
+            keycodes_ = rhs ;
+            return *this ;
+        }
+
+        const CmdUnit::Data& CmdUnit::get() const & noexcept {
+            return keycodes_ ;
+        }
+
+        const CmdUnit::Data& CmdUnit::data() const & noexcept {
+            return keycodes_ ;
+        }
 
         CmdUnit::Data::const_iterator CmdUnit::begin() const noexcept {
-            return pimpl->keys_.cbegin() ;
-        }
-        CmdUnit::Data::const_iterator end() const noexcept {
-            return pimpl->keys_.cend() ;
+            return keycodes_.begin() ;
         }
 
-        CmdUnit::Data::const_iterator cbegin() const noexcept {
-            return pimpl->keys_.cbegin() ;
+        CmdUnit::Data::const_iterator CmdUnit::end() const noexcept {
+            return keycodes_.end() ;
         }
-        CmdUnit::Data::const_iterator cend() const noexcept {
-            return pimpl->keys_.cend() ;
+
+        CmdUnit::Data::const_iterator CmdUnit::cbegin() const noexcept {
+            return keycodes_.cbegin() ;
+        }
+
+        CmdUnit::Data::const_iterator CmdUnit::cend() const noexcept {
+            return keycodes_.cend() ;
         }
 
         std::size_t CmdUnit::size() const noexcept {
-            return pimpl->keys.size() ;
+            return keycodes_.size() ;
         }
 
-        std::size_t CmdUnit::length() const noexcept {
-            return pimpl->keys.size() ;
+        bool CmdUnit::empty() const noexcept {
+            return keycodes_.empty() ;
         }
 
-        bool CmdUnit::for_external() const noexcept {
-            return pimpl->external_ ;
+        bool CmdUnit::is_containing(KeyCode key) const
+        {
+            return keycodes_.find(key) != keycodes_.end() ;
         }
 
-        bool CmdUnit::for_internal() const noexcept {
-            return !pimpl->external_ ;
+        bool CmdUnit::operator==(const CmdUnit& rhs) const {
+            return keycodes_ == rhs.keycodes_ ;
+        }
+        bool CmdUnit::operator==(CmdUnit&& rhs) const {
+            return keycodes_ == rhs.keycodes_ ;
+        }
+        bool CmdUnit::operator==(const CmdUnit::Data& rhs) const {
+            return keycodes_ == rhs ;
+        }
+        bool CmdUnit::operator==(CmdUnit::Data&& rhs) const {
+            return keycodes_ == rhs ;
+        }
+
+        bool CmdUnit::operator!=(const CmdUnit& rhs) const {
+            return keycodes_ != rhs.keycodes_ ;
+        }
+        bool CmdUnit::operator!=(CmdUnit&& rhs) const {
+            return keycodes_ != rhs.keycodes_ ;
+        }
+        bool CmdUnit::operator!=(const CmdUnit::Data& rhs) const {
+            return keycodes_ != rhs ;
+        }
+        bool CmdUnit::operator!=(CmdUnit::Data&& rhs) const {
+            return keycodes_ != rhs ;
+        }
+
+        const CmdUnit CmdUnit::operator-(const CmdUnit& rhs) const {
+            return CmdUnit(erased_diff(rhs.keycodes_)) ;
+        }
+        const CmdUnit CmdUnit::operator-(CmdUnit&& rhs) const {
+            return CmdUnit(erased_diff(std::move(rhs.keycodes_))) ;
+        }
+        const CmdUnit CmdUnit::operator-(const CmdUnit::Data& rhs) const {
+            return CmdUnit(erased_diff(rhs)) ;
+        }
+        const CmdUnit CmdUnit::operator-(CmdUnit::Data&& rhs) const {
+            return CmdUnit(erased_diff(std::move(rhs))) ;
+        }
+
+        CmdUnit& CmdUnit::operator-=(const CmdUnit& rhs) {
+            for(const auto& k : rhs) {
+                keycodes_.erase(k) ;
+            }
+            return *this ;
+        }
+        CmdUnit& CmdUnit::operator-=(CmdUnit&& rhs) {
+            for(const auto& k : rhs) {
+                keycodes_.erase(k) ;
+            }
+            return *this ;
+        }
+        CmdUnit& CmdUnit::operator-=(const CmdUnit::Data& rhs) {
+            for(const auto& k : rhs) {
+                keycodes_.erase(k) ;
+            }
+            return *this ;
+        }
+        CmdUnit& CmdUnit::operator-=(CmdUnit::Data&& rhs) {
+            for(const auto& k : rhs) {
+                keycodes_.erase(k) ;
+            }
+            return *this ;
         }
 
         void CmdUnit::execute() {
+        }
 
+        void InternalCmdUnit::execute() {
+        }
+
+        void ExternalCmdUnit::execute() {
+            // SendInput
+        }
+
+        bool FunctionalCmdUnit::has_function() const noexcept {
+            return func_ != nullptr ;
+        }
+
+        const std::shared_ptr<bind::BindedFunc>& FunctionalCmdUnit::get_function() const {
+            return func_ ;
+        }
+
+        void FunctionalCmdUnit::execute() {
+            if(!has_function()) {
+                throw RUNTIME_EXCEPT("Does not have an associated function.") ;
+            }
+            func_->process() ;
+        }
+
+
+        std::unique_ptr<CmdUnit> create_cmdunit(const std::string& strcommand) {
+            auto ptr = bind::ref_global_funcs_bynames(strcommand) ;
+            if(ptr) {
+                return std::make_unique<FunctionalCmdUnit>(std::move(ptr)) ;
+            }
+
+            auto cmd = parse_combined_command(strcommand) ;
+            return std::make_unique<InternalCmdUnit>(InternalCmdUnit::Data(cmd.begin(), cmd.end())) ;
         }
     }
 }
--- src/core/cmdunit.hpp
+++ src/core/cmdunit.hpp
@@ -3,32 +3,61 @@
 
 #include "keycode.hpp"
 
+#include <initializer_list>
 #include <memory>
 #include <string>
+#include <unordered_set>
 
 
 namespace vind
 {
+    namespace bind
+    {
+        class BindedFunc ;
+    }
+
     namespace core
     {
         class CmdUnit {
-        private:
-            struct Impl ;
-            std::unique_ptr<Impl> pimpl ;
-
         public:
             using Data = std::unordered_set<KeyCode> ;
+
+        private:
+            Data keycodes_ ;
+
+            template <typename T>
+            Data erased_diff(T&& rhs) const {
+                auto diff = keycodes_ ;
+                for(auto& k : rhs) {
+                    diff.erase(k) ;
+                }
+                return diff ;
+            }
+
+        public:
             explicit CmdUnit() ;
-            explicit CmdUnit(std::string&& strkeyset, bool external=false) ;
-            explicit CmdUnit(const std::string& strkeyset, bool external=false) ;
+            explicit CmdUnit(const Data& codes) ;
+            explicit CmdUnit(Data&& codes) ;
+            explicit CmdUnit(std::initializer_list<KeyCode>&& codes) ;
+
+            template <typename InputIterator>
+            explicit CmdUnit(InputIterator begin, InputIterator end)
+            : CmdUnit(Data(begin, end))
+            {}
 
             virtual ~CmdUnit() noexcept ;
 
-            CmdUnit(const CmdUnit&) = delete ;
-            CmdUnit& operator=(const CmdUnit&) = delete ;
-
             CmdUnit(CmdUnit&&) ;
             CmdUnit& operator=(CmdUnit&&) ;
+            CmdUnit& operator=(CmdUnit::Data&&) ;
+
+            CmdUnit(const CmdUnit&) ;
+            CmdUnit& operator=(const CmdUnit&) ;
+            CmdUnit& operator=(const CmdUnit::Data&) ;
+
+            const Data& get() const & noexcept ;
+
+            const Data& data() const & noexcept ;
 
             Data::const_iterator begin() const noexcept ;
             Data::const_iterator end() const noexcept ;
@@ -37,35 +66,71 @@ namespace vind
             Data::const_iterator cend() const noexcept ;
 
             std::size_t size() const noexcept ;
-            std::size_t length() const noexcept ;
+            bool empty() const noexcept ;
+            bool is_containing(KeyCode key) const ;
 
-            bool for_external() const noexcept ;
-            bool for_internal() const noexcept ;
+            bool operator==(const CmdUnit& rhs) const ;
+            bool operator==(CmdUnit&& rhs) const ;
+            bool operator==(const Data& rhsraw) const ;
+            bool operator==(Data&& rhsraw) const ;
 
-            void execute() ;
+            bool operator!=(const CmdUnit& rhs) const ;
+            bool operator!=(CmdUnit&& rhs) const ;
+            bool operator!=(const Data& rhs) const ;
+            bool operator!=(Data&& rhsw) const ;
+
+            const CmdUnit operator-(const CmdUnit& rhs) const ;
+            const CmdUnit operator-(CmdUnit&& rhs) const ;
+            const CmdUnit operator-(const Data& rhs) const ;
+            const CmdUnit operator-(Data&& rhs) const ;
+
+            CmdUnit& operator-=(const CmdUnit& rhs) ;
+            CmdUnit& operator-=(CmdUnit&& rhs) ;
+            CmdUnit& operator-=(const Data& rhs) ;
+            CmdUnit& operator-=(Data&& rhs) ;
+
+            virtual void execute() ;
         } ;
 
         class InternalCmdUnit : public CmdUnit {
-        private:
-
         public:
-            explicit InternalCmdUnit() ;
-            explicit InternalCmdUnit(std::string&& strkeyset, bool external=false) ;
-            explicit InternalCmdUnit(const std::string& strkeyset, bool external=false) ;
+            template <typename T>
+            explicit InternalCmdUnit(T&& keycodes)
+            : CmdUnit(std::forward<T>(keycodes))
+            {}
 
-            virtual ~InternalCmdUnit() noexcept ;
+            void execute() override ;
         } ;
 
         class ExternalCmdUnit : public CmdUnit {
+        public:
+            template <typename T>
+            explicit ExternalCmdUnit(T&& keycodes)
+            : CmdUnit(std::forward<T>(keycodes))
+            {}
+
+            void execute() override ;
+        } ;
+
+        class FunctionalCmdUnit : public CmdUnit {
         private:
+            std::shared_ptr<bind::BindedFunc> func_ ;
 
         public:
-            explicit ExternalCmdUnit() ;
-            explicit ExternalCmdUnit(std::string&& strkeyset, bool external=false) ;
-            explicit ExternalCmdUnit(const std::string& strkeyset, bool external=false) ;
+            template <typename T>
+            explicit FunctionalCmdUnit(T&& function_ptr)
+            : CmdUnit(),
+              func_(std::forward<T>(function_ptr))
+            {}
 
-            virtual ~ExternalCmdUnit() noexcept ;
+            bool has_function() const noexcept ;
+
+            const std::shared_ptr<bind::BindedFunc>& get_function() const ;
+
+            void execute() override ;
         } ;
+
+        std::unique_ptr<CmdUnit> create_cmdunit(const std::string& strcommand) ;
     }
 }
 

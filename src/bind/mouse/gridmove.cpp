334ee4e4f11cccb7c184c250e6fb80efa059c0d4
pit-ray <pit-ray@outlook.com>
2023-04-18 00:09:51 +0900

    fix the position of hints

--- src/bind/mouse/gridmove.cpp
+++ src/bind/mouse/gridmove.cpp
@@ -7,70 +7,64 @@
 #include "util/screen_metrics.hpp"
 #include "util/string.hpp"
 
+#include "core/inputhub.hpp"
 #include "core/settable.hpp"
 
 
-namespace vind
+namespace
 {
-    namespace bind
-    {
-        struct GridMove::Impl {
-            int l1_grid_w_ ;
-            int l1_grid_h_ ;
+    using namespace vind ;
 
-            util::DisplayTextPainter painter_ ;
-            util::DisplayTextPainter weak_painter_ ;
+    class TileHinter {
+    private:
+        int l1_grid_w_ ;
+        int l1_grid_h_ ;
 
-            Impl()
-            : l1_grid_w_(0),
-              l1_grid_h_(0),
-              painter_(0, 0, ""),
-              weak_painter_(painter_)
-            {}
-        } ;
+        util::DisplayTextPainter painter_ ;
+        util::DisplayTextPainter weak_painter_ ;
 
-        GridMove::GridMove()
-        : BindedFuncVoid("gridmove"),
-          pimpl(std::make_unique<Impl>())
+    public:
+        explicit TileHinter()
+        : l1_grid_w_(0),
+          l1_grid_h_(0),
+          painter_(0, 0, ""),
+          weak_painter_(painter_)
         {}
 
-        GridMove::~GridMove() noexcept = default ;
-        GridMove::GridMove(GridMove&&) = default ;
-        GridMove& GridMove::operator=(GridMove&&) = default ;
-
-        void GridMove::sprocess(
-                std::uint16_t UNUSED(count),
-                const std::string& UNUSED(args)) {
-
+        void draw_hints() {
             auto monitors = util::get_all_monitor_metrics() ;
             for(const auto& m : monitors) {
                 const auto& rect = m.rect ;
-                auto cell_w = rect.width() / pimpl->l1_grid_w_ ;
-                auto cell_h = rect.height() / pimpl->l1_grid_h_ ;
+                auto cell_w = rect.width() / l1_grid_w_ ;
+                auto cell_h = rect.height() / l1_grid_h_ ;
 
-                for(int j = 1 ; j <= pimpl->l1_grid_h_ ; j ++) {
-                    for(int i = 1 ; i <= pimpl->l1_grid_w_ ; i ++) {
+                // the center of the cell at (0, 0).
+                auto base_x = rect.left() + cell_w / 2 ;
+                auto base_y = rect.top() + cell_h / 2 ;
+
+                for(int j = 0 ; j < l1_grid_h_ ; j ++) {
+                    for(int i = 0 ; i < l1_grid_w_ ; i ++) {
                         util::Point2D pos{
-                            rect.left() + i * cell_w,
-                            rect.top() + j * cell_h
+                            base_x + i * cell_w,
+                            base_y + j * cell_h
                         } ;
 
-                        pimpl->painter_.draw("AA", pos, 1) ;
+                        painter_.draw("AA", pos, 1) ;
                     }
                 }
             }
 
-            pimpl->painter_.refresh() ;
+            painter_.refresh() ;
         }
 
-        void GridMove::reconstruct() {
+        void reconstruct() {
             auto& settable = core::SetTable::get_instance() ;
             auto l1_size = util::split(
                 settable.get("gridmove_l1size").get<std::string>(), "x") ;
-            pimpl->l1_grid_w_ = std::stoi(l1_size[0]) ;
-            pimpl->l1_grid_h_ = std::stoi(l1_size[1]) ;
+            l1_grid_w_ = std::stoi(l1_size[0]) ;
+            l1_grid_h_ = std::stoi(l1_size[1]) ;
 
-            std::cout << "L1 Grid Size: " << pimpl->l1_grid_w_ << " x " << pimpl->l1_grid_h_ << std::endl ;
+            std::cout << "L1 Grid Size: " << l1_grid_w_ << " x " << l1_grid_h_ << std::endl ;
 
             //Colors
             auto [bk_r, bk_g, bk_b] = util::hex2rgb(
@@ -90,16 +84,59 @@ namespace vind
                 (tx_g < decay) ? 0 : (tx_g + sign * decay),
                 (tx_b < decay) ? 0 : (tx_b + sign * decay)) ;
 
-            pimpl->painter_.set_font(
+            painter_.set_font(
                 settable.get("gridmove_fontsize").get<long>(),
                 settable.get("gridmove_fontweight").get<long>(),
                 settable.get("gridmove_fontname").get<std::string>()) ;
 
-            pimpl->painter_.set_back_color(bkcolor) ;
-            pimpl->painter_.set_text_color(txcolor) ;
+            painter_.set_back_color(bkcolor) ;
+            painter_.set_text_color(txcolor) ;
 
-            pimpl->weak_painter_ = pimpl->painter_ ;
-            pimpl->weak_painter_.set_text_color(txcolor_ready) ;
+            weak_painter_ = painter_ ;
+            weak_painter_.set_text_color(txcolor_ready) ;
+        }
+    } ;
+}
+
+
+namespace vind
+{
+    namespace bind
+    {
+        struct GridMove::Impl {
+            TileHinter tile_hinter_{} ;
+        } ;
+
+        GridMove::GridMove()
+        : BindedFuncVoid("gridmove"),
+          pimpl(std::make_unique<Impl>())
+        {}
+
+        GridMove::~GridMove() noexcept = default ;
+        GridMove::GridMove(GridMove&&) = default ;
+        GridMove& GridMove::operator=(GridMove&&) = default ;
+
+        void GridMove::sprocess(
+                std::uint16_t UNUSED(count),
+                const std::string& UNUSED(args)) {
+            auto& ihub = core::InputHub::get_instance() ;
+
+            while(true) {
+                pimpl->tile_hinter_.draw_hints() ;
+
+                core::CmdUnit::SPtr inputs ;
+                std::uint16_t count ;
+                if(!ihub.fetch_input(inputs, count, core::get_global_mode(), false)) {
+                    continue ;
+                }
+                if(inputs->is_containing(KEYCODE_ESC)) {
+                    break ;
+                }
+            }
+        }
+
+        void GridMove::reconstruct() {
+            pimpl->tile_hinter_.reconstruct() ;
         }
     }
 }
